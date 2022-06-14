1f3328b044fbdc379d4e6d0a747952052345e28e
Ryooooooga <eial5q265e5@gmail.com>
2022-06-14 23:49:29 +0900

    chore: remove `dirs` crate

--- src/config/config_path.rs
+++ src/config/config_path.rs
@@ -4,13 +4,13 @@ use std::path::PathBuf;
 
 static ZABRZE_CONFIG_FILE_ENV_KEY: &str = "ZABRZE_CONFIG_FILE";
 static XDG_CONFIG_HOME_ENV_KEY: &str = "XDG_CONFIG_HOME";
+static HOME_ENV_KEY: &str = "HOME";
 
 static DEFAULT_CONFIG_DIR: &str = "zabrze";
 static DEFAULT_CONFIG_FILE: &str = "config.yaml";
 
 trait ConfigPath {
     fn env(&self, key: &str) -> Option<OsString>;
-    fn home(&self) -> Option<PathBuf>;
 }
 
 #[derive(Debug)]
@@ -19,9 +19,6 @@ struct ConfigPathImpl {}
 impl ConfigPath for ConfigPathImpl {
     fn env(&self, key: &str) -> Option<OsString> {
         env::var_os(key)
-    }
-    fn home(&self) -> Option<PathBuf> {
-        dirs::home_dir()
     }
 }
 
@@ -36,7 +33,7 @@ fn get_default_path<C: ConfigPath>(c: &C) -> Option<PathBuf> {
         if let Some(xdg_config_home) = c.env(XDG_CONFIG_HOME_ENV_KEY).map(PathBuf::from) {
             xdg_config_home
         } else {
-            let mut path = c.home()?;
+            let mut path = c.env(HOME_ENV_KEY).map(PathBuf::from)?;
             path.push(".config");
             path
         };
@@ -60,15 +57,11 @@ mod tests {
     #[derive(Debug)]
     struct DummyConfigPath {
         envs: HashMap<&'static str, &'static str>,
-        home: &'static str,
     }
 
     impl ConfigPath for DummyConfigPath {
         fn env(&self, key: &str) -> Option<OsString> {
             self.envs.get(key).map(OsString::from)
-        }
-        fn home(&self) -> Option<PathBuf> {
-            Some(PathBuf::from(self.home))
         }
     }
 
@@ -77,7 +70,6 @@ mod tests {
         struct Scenario {
             pub testname: &'static str,
             pub envs: HashMap<&'static str, &'static str>,
-            pub home: &'static str,
             pub expected: &'static str,
         }
 
@@ -87,24 +79,25 @@ mod tests {
                 envs: vec![
                     ("ZABRZE_CONFIG_FILE", "/home/user/.zabrze.yaml"),
                     ("XDG_CONFIG_HOME", "/home/user/.xdgConfig"),
+                    ("HOME", "/home/user"),
                 ]
                 .into_iter()
                 .collect(),
-                home: "/home/user/",
                 expected: "/home/user/.zabrze.yaml",
             },
             Scenario {
                 testname: "follow XDG_CONFIG_HOME",
-                envs: vec![("XDG_CONFIG_HOME", "/home/user/.xdgConfig")]
-                    .into_iter()
-                    .collect(),
-                home: "/home/user/",
+                envs: vec![
+                    ("XDG_CONFIG_HOME", "/home/user/.xdgConfig"),
+                    ("HOME", "/home/user"),
+                ]
+                .into_iter()
+                .collect(),
                 expected: "/home/user/.xdgConfig/zabrze/config.yaml",
             },
             Scenario {
                 testname: "use default path",
-                envs: HashMap::new(),
-                home: "/home/user/",
+                envs: vec![("HOME", "/home/user")].into_iter().collect(),
                 expected: "/home/user/.config/zabrze/config.yaml",
             },
         ];
@@ -112,7 +105,6 @@ mod tests {
         for s in &scenarios {
             let c = DummyConfigPath {
                 envs: s.envs.clone(),
-                home: s.home,
             };
 
             let expected = Some(PathBuf::from(s.expected));
