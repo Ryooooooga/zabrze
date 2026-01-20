de6760331df23b26a9394d7568912cd0885e2f6c
Ryooooooga <eial5q265e5@gmail.com>
2026-01-20 14:18:51 +0900

    feat(config): support TOML configuration in tests

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,17 +7,20 @@ pub use config_path::get_default_config_dir;
 use ansi_term::Color;
 use serde::{Deserialize, Serialize};
 use std::fs::{File, read_dir};
-use std::io;
+use std::io::{self, Read};
 use std::path::{Path, PathBuf};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ConfigError {
     #[error(transparent)]
-    IoError(#[from] io::Error),
+    Io(#[from] io::Error),
 
     #[error(transparent)]
-    YamlError(#[from] serde_yaml::Error),
+    Toml(#[from] toml::de::Error),
+
+    #[error(transparent)]
+    Yaml(#[from] serde_yaml::Error),
 }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -28,13 +31,20 @@ pub struct Config {
 impl Config {
     #[cfg(test)]
     pub fn load_from_str(s: &str) -> Result<Self, ConfigError> {
-        let config = serde_yaml::from_str(s)?;
+        let config = toml::from_str(s)?;
         Ok(config)
     }
 
-    pub fn load_from_file<P: AsRef<Path>>(path: P) -> Result<Self, ConfigError> {
-        let file = File::open(path)?;
-        let config = serde_yaml::from_reader(&file)?;
+    pub fn load_from_file(path: &Path) -> Result<Self, ConfigError> {
+        let mut file = File::open(path)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+
+        let config = if path.extension() == Some("toml".as_ref()) {
+            toml::from_slice(&buffer)?
+        } else {
+            serde_yaml::from_slice(&buffer)?
+        };
         Ok(config)
     }
 
@@ -72,7 +82,7 @@ impl Config {
             if entry.file_type()?.is_file() {
                 let path = entry.path();
                 if let Some(ext) = path.extension()
-                    && (ext == "yaml" || ext == "yml")
+                    && (ext == "toml" || ext == "yaml" || ext == "yml")
                 {
                     paths.push(path);
                 }
